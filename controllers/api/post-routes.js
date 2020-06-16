8c246f75bc05cf070dc2e9b1a7c543695598de62
jpd61 <jpd@dewoodyoil.com>
2020-06-15 20:26:18 -0500

    updates and improvements

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -92,7 +92,8 @@ router.post('/', withAuth, (req, res) => {
 
 router.put('/:id', withAuth, (req, res) => {
     Post.update({
-        title: req.body.title
+        title: req.body.title,
+        post_content: req.body.post_content
       },
       {
         where: {
